Major changes: (i) restructured scipy minimize formulation to use constraints and bounds. (ii) removed p_transform feature for the moment. (iii) identified bug in fim scaling from last commit, removed at the moment. (iv) changed plot current design to only show optimal candidates. (v) sped-up computation of the fim, important for larger number of candidates. This is done by changing .dot operator in numpy to @ operator. Also, reduced RAM requirement by opting for loops and removing the np.sum() and using list comprehension. Surprisingly, this for loop method for fim is much faster than using np.sum and list comprehension, extensive testing was done. (vi) force_3d for plotting feature will only work if the model is dynamic, added warning when user force3d on non-dynamic models. (vii) added two extra attributes that are saved when save_state is called, this fixes bug when saving and loading. (viii) added A-criterion for use with cvxpy.
Change for examples: (i) tweaked polynomial examples to include 3-factor interactions; (ii) polishing results presentation; (iii) polishing code structure for illustrating optimization package, optimizer, and criterion choices; (iv) corrected example for exponential regression order 7. (v) corrected errors in oed example for loading results for case 1.

Code refactoring: (i) removed unimplemented features at the moment. (ii) prediction variances, and information criteria based on prediction removed at the moment. (iii) renamed get_sensitivities, get_residuals to eval_sensitivities, eval_residuals respectively. (iv) restructured designer' methods into core interfaces, utilities, evaluators, getters, optional, and private. (v) changes assertions to if ____: raise SyntaxError where ever appropriate, this is to stress that the user has made a syntax error when defining the optimal design problem to the designer; assertions alone are unclear, and having to manually repeat Syntax error every time assertion occurs seems inefficient. (vi) renam

examples/linear/order_1_polynomial_2_inputs.py
@@ -5,33 +5,28 @@ import numpy as np
 
 """ 
 Setting: a non-dynamic experimental system with 2 time-invariant control variables and 1 response.
-Problem: develop a statistical model using an exponential regression model. Use integer values for the exponential
-         growth rates between (and including) -2 and 2. 
-Solution: non-standard design
+Problem: design optimal experiment for a order 1 polynomial, with complete interaction
+Solution: a full 2^2 factorial design (2 level)
 """
 def simulate(ti_controls, tv_controls, model_parameters, sampling_times):
     return np.array([
-        model_parameters[0]                                     +
-
-        model_parameters[1] * np.exp(-1 * ti_controls[0])       +
-        model_parameters[2] * np.exp( 1 * ti_controls[0])       +
-        model_parameters[3] * np.exp(-2 * ti_controls[0])       +
-        model_parameters[4] * np.exp( 2 * ti_controls[0])       +
-
-        model_parameters[5] * np.exp(-1 * ti_controls[1])       +
-        model_parameters[6] * np.exp( 1 * ti_controls[1])       +
-        model_parameters[7] * np.exp(-2 * ti_controls[1])       +
-        model_parameters[8] * np.exp( 2 * ti_controls[1])
+        # constant term
+        model_parameters[0] +
+        # linear term
+        model_parameters[1] * ti_controls[0]                    +
+        model_parameters[2] * ti_controls[1]                    +
+        # interaction term
+        model_parameters[3] * ti_controls[0] * ti_controls[1]
     ])
 
 designer_1 = Designer()
 designer_1.simulate = simulate
 
-tic_1, tic_2 = np.mgrid[-1:1:21j, -1:1:21j]
+tic_1, tic_2 = np.mgrid[-1:1:11j, -1:1:11j]
 tic_1 = tic_1.flatten(); tic_2 = tic_2.flatten()
 designer_1.ti_controls_candidates = np.array([tic_1, tic_2]).T
 
-designer_1.model_parameters = np.ones(9)  # values won't affect design, but still needed
+designer_1.model_parameters = np.ones(4)  # values won't affect design, but still needed
 
 designer_1.initialize(verbose=2)  # 0: silent, 1: overview, 2: detailed, 3: very detailed
 

examples/linear/order_5_exponential_regression.py
@@ -27,16 +27,20 @@ tic_1, tic_2 = np.mgrid[-1:1:11j, -1:1:11j]
 tic_1 = tic_1.flatten(); tic_2 = tic_2.flatten()
 designer_1.ti_controls_candidates = np.array([tic_1, tic_2]).T
 
-designer_1.model_parameters = np.array([1, 2, 3, 4, 5])  # values won't affect design, but still needed
+designer_1.model_parameters = np.ones(5)  # values won't affect design, but still needed
 
-designer_1.initialize(verbose=2)
+designer_1.initialize(verbose=2)  # 0: silent, 1: overview, 2: detailed, 3: very detailed
 
-designer_1.design_experiment(designer_1.d_opt_criterion)
+package, optimizer = ("cvxpy", "MOSEK")
+# package, optimizer = ("cvxpy", "SCS")
+# package, optimizer = ("scipy", "SLSQP")
+designer_1.design_experiment(designer_1.d_opt_criterion, package=package, optimizer=optimizer, write=False)
 designer_1.print_optimal_candidates()
+designer_1.plot_current_design()
 
 fig1 = plt.figure()
 axes1 = fig1.add_subplot(111)
 axes1.scatter(designer_1.ti_controls_candidates[:, 0], designer_1.ti_controls_candidates[:, 1],
-              s=designer_1.efforts*1000)
+              s=np.round(designer_1.efforts*1000, decimals=2))
 plt.show()
 

examples/ode/ode_oed_case_2.py
@@ -133,41 +133,31 @@ optional, if un-specified assume all responses (from simulate function) measurab
 # designer_1.measurable_responses = [0, 1]
 
 """ initializing designer """
-designer_1.initialize(verbose=1)  # 0: silent, 1: overview, 2: detail
-designer_1.responses_scales = np.array([1, 1])
+designer_1.initialize(verbose=2)  # 0: silent, 1: overview, 2: detail
+# designer_1.responses_scales = np.array([1, 1])
 
-designer_1.estimability_study_fim()
-designer_1.candidate_names = np.array(["Candidate {0:d}".format(i+1) for i, _ in enumerate(tic_candidates)])
+# designer_1.estimability_study_fim()
+# designer_1.candidate_names = np.array(["Candidate {0:d}".format(i+1) for i, _ in enumerate(tic_candidates)])
 
 """ D-optimal design """
 package, optimizer = ("cvxpy", "MOSEK")
 # package, optimizer = ("cvxpy", "SCS")
-# package, optimizer = ("scipy", "bfgs")
+# package, optimizer = ("cvxpy", "CVXOPT")
 # package, optimizer = ("scipy", "SLSQP")
-d_opt_result = designer_1.design_experiment(criterion=designer_1.d_opt_criterion, package=package, optimizer=optimizer,
-                                            plot=False, optimize_sampling_times=True, write=False)
+
+criterion = designer_1.d_opt_criterion
+# criterion = designer_1.a_opt_criterion
+# criterion = designer_1.e_opt_criterion
+
+result = designer_1.design_experiment(criterion=criterion, package=package, optimizer=optimizer,
+                                      plot=False, optimize_sampling_times=True, write=False, save_sensitivities=True)
+
 designer_1.print_optimal_candidates()
 designer_1.plot_optimal_predictions()
 designer_1.plot_optimal_sensitivities(absolute=True)
 designer_1.plot_current_design()
+designer_1.simulate_all_candidates(plot_simulation_times=True)
+designer_1.plot_all_predictions()
 
-# e_opt_result = designer_1.design_experiment(criterion=designer_1.e_opt_criterion, package=package, optimizer=optimizer,
-#                                             plot=False, optimize_sampling_times=True, write=False)
-# designer_1.print_optimal_candidates()
-# # designer_1.plot_optimal_predictions()
-# designer_1.plot_optimal_sensitivities(absolute=True)
-# designer_1.plot_current_design()
-
-# a_opt_result = designer_1.design_experiment(criterion=designer_1.a_opt_criterion, package='scipy', optimizer='SLSQP',
-#                                             plot=False, optimize_sampling_times=True, write=False,
-#                                             opt_options={"disp": True, "maxiter": 1e6})
-# designer_1.print_optimal_candidates()
-# # designer_1.plot_optimal_predictions()
-# designer_1.plot_optimal_sensitivities(absolute=True)
-# designer_1.plot_current_design()
-
-# """ an option for saving current designer and the specified candidates """
-# designer_1.save()
-#
-# designer_1.simulate_all_candidates(plot_simulation_times=True)
-# designer_1.plot_all_predictions()
+""" an option for saving current designer and the specified candidates """
+designer_1.save_state()