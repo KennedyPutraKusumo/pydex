287ea4702c9e4d5136d38d4a08f4d14d735b8e2f
KennedyPutraKusumo <nedy.fly@gmail.com>
2020-04-07 18:47:39 +0100

    Weeded out examples from wrong comments and code rearranging for tidiness.

--- examples/linear/order_5_exponential_regression.py
+++ examples/linear/order_5_exponential_regression.py
@@ -1,7 +1,7 @@
-from core.designer import Designer
-from matplotlib import pyplot as plt
 import numpy as np
+from matplotlib import pyplot as plt
 
+from core.designer import Designer
 
 """ 
 Setting: a non-dynamic experimental system with 2 time-invariant control variables and 1 response.
@@ -9,22 +9,26 @@ Problem: develop a statistical model using an exponential regression model. Use 
          growth rates between (and including) -1 and 1. 
 Solution: a standard 3^2 factorial design
 """
+
+
 def simulate(ti_controls, tv_controls, model_parameters, sampling_times):
     return np.array([
-        model_parameters[0]                                     +
+        model_parameters[0] +
 
-        model_parameters[1] * np.exp(-1 * ti_controls[0])       +
-        model_parameters[2] * np.exp( 1 * ti_controls[0])       +
+        model_parameters[1] * np.exp(-1 * ti_controls[0]) +
+        model_parameters[2] * np.exp(1 * ti_controls[0]) +
 
-        model_parameters[3] * np.exp(-1 * ti_controls[1])       +
-        model_parameters[4] * np.exp( 1 * ti_controls[1])
+        model_parameters[3] * np.exp(-1 * ti_controls[1]) +
+        model_parameters[4] * np.exp(1 * ti_controls[1])
     ])
+
 
 designer_1 = Designer()
 designer_1.simulate = simulate
 
 tic_1, tic_2 = np.mgrid[-1:1:11j, -1:1:11j]
-tic_1 = tic_1.flatten(); tic_2 = tic_2.flatten()
+tic_1 = tic_1.flatten()
+tic_2 = tic_2.flatten()
 designer_1.ti_controls_candidates = np.array([tic_1, tic_2]).T
 
 designer_1.model_parameters = np.ones(5)  # values won't affect design, but still needed
@@ -37,7 +41,7 @@ designer_1.plot_current_design()
 
 fig1 = plt.figure()
 axes1 = fig1.add_subplot(111)
-axes1.scatter(designer_1.ti_controls_candidates[:, 0], designer_1.ti_controls_candidates[:, 1],
-              s=np.round(designer_1.efforts*1000, decimals=2))
+axes1.scatter(designer_1.ti_controls_candidates[:, 0],
+              designer_1.ti_controls_candidates[:, 1],
+              s=np.round(designer_1.efforts * 1000, decimals=2))
 plt.show()
-
--- examples/linear/order_7_exponential_regression.py
+++ examples/linear/order_7_exponential_regression.py
@@ -1,7 +1,7 @@
-from core.designer import Designer
-from matplotlib import pyplot as plt
 import numpy as np
+from matplotlib import pyplot as plt
 
+from core.designer import Designer
 
 """ 
 Setting: a non-dynamic experimental system with 2 time-invariant control variables and 1 response.
@@ -9,26 +9,30 @@ Problem: develop a statistical model using an exponential regression model. Use 
          growth rates between (and including) -2 and 2. 
 Solution: non-standard design
 """
+
+
 def simulate(ti_controls, tv_controls, model_parameters, sampling_times):
     return np.array([
-        model_parameters[0]                                     +
+        model_parameters[0] +
 
-        model_parameters[1] * np.exp(-1 * ti_controls[0])       +
-        model_parameters[2] * np.exp( 1 * ti_controls[0])       +
-        model_parameters[3] * np.exp(-2 * ti_controls[0])       +
-        model_parameters[4] * np.exp( 2 * ti_controls[0])       +
+        model_parameters[1] * np.exp(-1 * ti_controls[0]) +
+        model_parameters[2] * np.exp(1 * ti_controls[0]) +
+        model_parameters[3] * np.exp(-2 * ti_controls[0]) +
+        model_parameters[4] * np.exp(2 * ti_controls[0]) +
 
-        model_parameters[5] * np.exp(-1 * ti_controls[1])       +
-        model_parameters[6] * np.exp( 1 * ti_controls[1])       +
-        model_parameters[7] * np.exp(-2 * ti_controls[1])       +
-        model_parameters[8] * np.exp( 2 * ti_controls[1])
+        model_parameters[5] * np.exp(-1 * ti_controls[1]) +
+        model_parameters[6] * np.exp(1 * ti_controls[1]) +
+        model_parameters[7] * np.exp(-2 * ti_controls[1]) +
+        model_parameters[8] * np.exp(2 * ti_controls[1])
     ])
+
 
 designer_1 = Designer()
 designer_1.simulate = simulate
 
-tic_1, tic_2 = np.mgrid[-1:1:21j, -1:1:21j]
-tic_1 = tic_1.flatten(); tic_2 = tic_2.flatten()
+tic_1, tic_2 = np.mgrid[-1:1:11j, -1:1:11j]
+tic_1 = tic_1.flatten()
+tic_2 = tic_2.flatten()
 designer_1.ti_controls_candidates = np.array([tic_1, tic_2]).T
 
 designer_1.model_parameters = np.ones(9)  # values won't affect design, but still needed
@@ -41,6 +45,7 @@ designer_1.plot_current_design()
 
 fig1 = plt.figure()
 axes1 = fig1.add_subplot(111)
-axes1.scatter(designer_1.ti_controls_candidates[:, 0], designer_1.ti_controls_candidates[:, 1],
-              s=np.round(designer_1.efforts*1000, decimals=2))
+axes1.scatter(designer_1.ti_controls_candidates[:, 0],
+              designer_1.ti_controls_candidates[:, 1],
+              s=np.round(designer_1.efforts * 1000, decimals=2))
 plt.show()
