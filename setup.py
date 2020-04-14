5f5c88e09068eeeed4f6efdce5c018226f3501eb
KennedyPutraKusumo <kennedy.putra.kusumo@gmail.com>
2020-04-14 15:35:10 +0100

    deleting .idea

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pydex",
+    version="0.0.3",
+    author="Kennedy Putra Kusumo",
+    author_email="kennedy.putra.kusumo@gmail.com",
+    description="An open-source package for optimal continuous experiment design using the discrete method.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/KennedyPutraKusumo/pydex",
+    packages=setuptools.find_packages(exclude=["examples", "examples.*"]),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
