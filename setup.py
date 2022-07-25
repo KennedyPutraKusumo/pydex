2d9001b33b7ff4a38fe8b3aa245daaa92e1aac43
KennedyPutraKusumo <kennedy.putra.kusumo@gmail.com>
2022-07-25 14:16:37 +0100

    added dependencies on setup.py for pypi installations

--- setup.py
+++ setup.py
@@ -19,4 +19,13 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    install_requires=[
+        'matplotlib',
+        'numdifftools',
+        'scipy',
+        'cvxpy',
+        'pickle',
+        'dill',
+        'numpy',
+    ],
 )
