d1a39ae1f7d1e89cbb5bfc4b88493e9bac46b41d
KennedyPutraKusumo <kennedy.putra.kusumo@gmail.com>
2022-08-09 11:47:04 +0100

    updated setup.py to 0.0.9

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pydex",
-    version="0.0.8",
+    version="0.0.9",
     author="Kennedy Putra Kusumo",
     author_email="kennedy.putra.kusumo@gmail.com",
     description="A Python package for optimal experiment design.",
